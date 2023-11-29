9af6910357640c524c5c3a41d43ed0902d6a9502
Nikita <np.shche@gmail.com>
2023-11-30 02:15:46 +0300

    initial commit

---
+++ Assets/Scripts/MapBuilder.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour
+{
+    /// <summary>
+    /// Данный метод вызывается автоматически при клике на кнопки с изображениями тайлов.
+    /// В качестве параметра передается префаб тайла, изображенный на кнопке.
+    /// Вы можете использовать префаб tilePrefab внутри данного метода.
+    /// </summary>
+    public void StartPlacingTile(GameObject tilePrefab)
+    {
+        
+    }
+}
